f2aec4e838619a4bb99cedc7cefb0fb952eb2bb8
gunal-123 <103403219+gunal-123@users.noreply.github.com>
2022-07-12 13:46:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "gunalbucket"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "ap-south-1"
+    dynamodb_table = "gunaltable"
   }
 }
